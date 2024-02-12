19c75be4d3aa58d2118b78227cbcd322d29a567f
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-02-12 12:41:51 -0300

    Updating about page

--- src/Views/About/index.tsx
+++ src/Views/About/index.tsx
@@ -1,74 +1,185 @@
-import React, { useCallback } from 'react';
-import { Linking } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { getVersion } from 'react-native-device-info';
-
-import strings from '~/Locales';
+import React, { useState, useCallback, useEffect } from 'react';
+import { View, Linking } from 'react-native';
+import {
+	getVersion,
+	getBuildNumber,
+	getSystemName,
+	getSystemVersion,
+} from 'react-native-device-info';
+import messaging from '@react-native-firebase/messaging';
+import Purchases from 'react-native-purchases';
 
 import Header from '~/Components/Header';
 
 import {
 	Container,
 	Content,
-	PageTitle,
+	ApplicationName,
 	ApplicationVersion,
 	AboutSection,
-	ApplicationName,
+	UserId,
 	Text,
 	Link,
+	SocialContainer,
+	SocialIcon,
+	IdContainer,
+	IdButton,
 } from './styles';
 
 const About: React.FC = () => {
-	const { goBack } = useNavigation();
+	const [pid, setPid] = useState('');
+	const [firebaseId, setFirebaseId] = useState('');
 
-	const handleLinkedinPress = useCallback(async () => {
-		await Linking.openURL('https://www.linkedin.com/in/douglasndm/');
+	const loadData = useCallback(async () => {
+		try {
+			const purchase = await Purchases.getAppUserID();
+
+			setPid(purchase);
+		} catch (err) {
+			if (err instanceof Error) {
+				console.log(err);
+			}
+		}
+
+		try {
+			const firebase = await messaging().getToken();
+
+			setFirebaseId(firebase);
+		} catch (err) {
+			if (err instanceof Error) {
+				console.log(err);
+			}
+		}
 	}, []);
 
-	const iconsUrl = useCallback(async () => {
-		await Linking.openURL('https://www.flaticon.com/authors/smashicons');
+	useEffect(() => {
+		loadData();
+	}, []);
+
+	const handleNavigateToSite = useCallback(async () => {
+		await Linking.openURL('https://douglasndm.dev');
 	}, []);
 
 	const navigateToTerms = useCallback(async () => {
-		await Linking.openURL('https://douglasndm.dev/terms');
+		await Linking.openURL('https://douglasndm.dev/terms/expiry-tracker');
 	}, []);
 
 	const navigateToPrivacy = useCallback(async () => {
 		await Linking.openURL('https://douglasndm.dev/privacy');
 	}, []);
 
+	const handleNaviTelegram = useCallback(async () => {
+		await Linking.openURL('https://t.me/douglasdev');
+	}, []);
+
+	const handleLinkedinPress = useCallback(async () => {
+		await Linking.openURL('https://www.linkedin.com/in/douglasndm/');
+	}, []);
+
+	const handleNaviTwitter = useCallback(async () => {
+		await Linking.openURL('https://www.twitter.com/douglasndmdev/');
+	}, []);
+
+	const handleNaviMail = useCallback(async () => {
+		if (await Linking.canOpenURL('mailto:suporte@douglasndm.dev'))
+			await Linking.openURL('mailto:suporte@douglasndm.dev');
+	}, []);
+
+	const handleFlatIconPress = useCallback(async () => {
+		await Linking.openURL('https://www.flaticon.com/authors/srip');
+	}, []);
+
+	const handleFlatIconPROIconPress = useCallback(async () => {
+		await Linking.openURL(
+			'https://www.flaticon.com/free-icons/high-quality'
+		);
+	}, []);
+
 	return (
 		<Container>
-			<Header title="Sobre" />
+			<Header title="Sobre" noDrawer />
+			<Content>
+				<AboutSection>
+					<ApplicationName>Tabuada</ApplicationName>
 
-			<AboutSection>
-				<ApplicationName>Tabuada</ApplicationName>
-				<ApplicationVersion>
-					{`Versão: ${getVersion()}`}
-				</ApplicationVersion>
-			</AboutSection>
+					<View style={{ flexDirection: 'row' }}>
+						<ApplicationVersion>
+							{`Versão ${getVersion()} (Build: ${getBuildNumber()})`}
+						</ApplicationVersion>
+					</View>
+					<ApplicationVersion>{`${getSystemName()} ${getSystemVersion()}`}</ApplicationVersion>
+				</AboutSection>
 
-			<AboutSection>
-				<Text>Criado por Douglas Nunes de Mattos</Text>
+				{(!!firebaseId || !!pid) && (
+					<IdContainer>
+						<IdButton>
+							<View>
+								<UserId>{`mid: ${firebaseId}`}</UserId>
+								<UserId>{`pid: ${pid}`}</UserId>
+							</View>
+						</IdButton>
+					</IdContainer>
+				)}
 
-				<Link onPress={handleLinkedinPress}>Linkedin</Link>
-			</AboutSection>
+				<AboutSection>
+					<Text>
+						Ao usar este aplicativo você está aceitando nossos
+						<Link onPress={navigateToTerms}> Termos de uso</Link>
+						{` e `}
+						<Link onPress={navigateToPrivacy}>
+							Política de Privacidade
+						</Link>
+						.
+					</Text>
+				</AboutSection>
 
-			<AboutSection>
-				<Text>Icons made by Smashicons from flaticon.com</Text>
-			</AboutSection>
+				<AboutSection>
+					<Text>Logo feito por</Text>
 
-			<AboutSection>
-				<Text>
-					{strings.BeforeTermsAndPrivacy}
-					<Link onPress={navigateToTerms}>{strings.Terms}</Link>
-					{strings.BetweenTermsAndPrivacy}
-					<Link onPress={navigateToPrivacy}>
-						{strings.PrivacyPolicy}
-					</Link>
-					.
-				</Text>
-			</AboutSection>
+					<View>
+						<Link onPress={handleFlatIconPress}>
+							https://www.flaticon.com/authors/srip
+						</Link>
+					</View>
+				</AboutSection>
+
+				<AboutSection>
+					<Text>
+						High quality icons created by HAJICON - Flaticon
+					</Text>
+					<View>
+						<Link onPress={handleFlatIconPROIconPress}>
+							https://www.flaticon.com/free-icons/high-quality
+						</Link>
+					</View>
+				</AboutSection>
+
+				<AboutSection>
+					<Text>Criado por Douglas Nunes de Mattos</Text>
+				</AboutSection>
+
+				<SocialContainer>
+					<SocialIcon
+						name="desktop-outline"
+						onPress={handleNavigateToSite}
+					/>
+
+					<SocialIcon
+						name="chatbubbles-outline"
+						onPress={handleNaviTelegram}
+					/>
+
+					<SocialIcon
+						name="logo-linkedin"
+						onPress={handleLinkedinPress}
+					/>
+					<SocialIcon
+						name="logo-twitter"
+						onPress={handleNaviTwitter}
+					/>
+					<SocialIcon name="mail-outline" onPress={handleNaviMail} />
+				</SocialContainer>
+			</Content>
 		</Container>
 	);
 };
--- src/Views/About/styles.ts
+++ src/Views/About/styles.ts
@@ -1,49 +1,59 @@
 import styled from 'styled-components/native';
-import { Platform } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export const Container = styled.View`
-    flex: 1;
-    padding: ${Platform.OS === 'ios' ? 50 : 16}px 10px 5px 10px;
-    background: ${(props) => props.theme.colors.background};
+	flex: 1;
+	background: ${props => props.theme.colors.background};
 `;
 
-export const Content = styled.View`
-    flex-direction: row;
-    margin-left: -15px;
-`;
-
-export const PageTitle = styled.Text`
-    font-size: 28px;
-    font-weight: bold;
-    color: ${(props) => props.theme.colors.text};
-`;
+export const Content = styled.ScrollView``;
 
 export const AboutSection = styled.View`
-    margin-top: 20px;
+	margin: 20px 10px 0 10px;
 `;
 
 export const ApplicationName = styled.Text`
-    font-size: 24px;
-    font-weight: bold;
-    color: ${(props) => props.theme.colors.text};
-`;
-
-export const UserId = styled.Text`
-    margin-top: 5px;
-    color: ${(props) => props.theme.colors.text};
+	font-size: 24px;
+	font-weight: bold;
+	color: ${props => props.theme.colors.text};
 `;
 
 export const ApplicationVersion = styled.Text`
-    font-size: 14px;
-    color: ${(props) => props.theme.colors.subText};
+	font-size: 14px;
+	color: ${props => props.theme.colors.subText};
+`;
+
+export const IdContainer = styled.View`
+	margin: 0 10px 0 10px;
+`;
+export const IdButton = styled.Pressable``;
+
+export const UserId = styled.Text.attrs(() => ({
+	numberOfLines: 1,
+}))`
+	font-size: 14px;
+	color: ${props => props.theme.colors.subText};
 `;
 
 export const Text = styled.Text`
-    color: ${(props) => props.theme.colors.text};
-    font-size: 16px;
+	color: ${props => props.theme.colors.text};
+	font-size: 16px;
 `;
 
 export const Link = styled.Text`
-    color: ${(props) => props.theme.colors.accent};
-    font-size: 14px;
+	color: ${props => props.theme.colors.accent};
+	font-size: 14px;
+`;
+
+export const SocialContainer = styled.View`
+	margin-top: 25px;
+	justify-content: center;
+	flex-direction: row;
+`;
+
+export const SocialIcon = styled(Ionicons).attrs(props => ({
+	size: 36,
+	color: props.theme.colors.text,
+}))`
+	margin-right: 15px;
 `;
