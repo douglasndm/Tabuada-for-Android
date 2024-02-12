04b8c59fd59f87836a57af82a431d4a7e0d3e019
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-02-12 12:45:43 -0300

    Making button round

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -4,28 +4,28 @@ import { ViewStyle } from 'react-native';
 import { Button, ButtonText, Loading } from './styles';
 
 interface Request {
-    text: string;
-    accessibilityLabel?: string;
-    isLoading?: boolean;
-    onPress: () => void;
-    contentStyle?: ViewStyle;
+	text: string;
+	accessibilityLabel?: string;
+	isLoading?: boolean;
+	onPress: () => void;
+	contentStyle?: ViewStyle;
 }
 
 const GenericButton: React.FC<Request> = ({
-    text,
-    accessibilityLabel = '',
-    isLoading = false,
-    onPress,
-    contentStyle,
+	text,
+	accessibilityLabel = '',
+	isLoading = false,
+	onPress,
+	contentStyle,
 }: Request) => (
-    <Button
-        onPress={onPress}
-        enabled={!isLoading}
-        accessibilityLabel={accessibilityLabel}
-        style={contentStyle}
-    >
-        {isLoading ? <Loading /> : <ButtonText>{text}</ButtonText>}
-    </Button>
+	<Button
+		onPress={onPress}
+		enabled={!isLoading}
+		accessibilityLabel={accessibilityLabel}
+		style={contentStyle}
+	>
+		{isLoading ? <Loading /> : <ButtonText>{text}</ButtonText>}
+	</Button>
 );
 
 export default GenericButton;
